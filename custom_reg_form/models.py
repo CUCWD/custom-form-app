9347711735b18f87c1104758bbb783e2def6c541
Rebecca Callis <rldavid@clemson.edu>
2025-04-29 08:34:22 -0400

    fix: ugettext_noop depreciated in django 3.0

--- custom_reg_form/models.py
+++ custom_reg_form/models.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.db import models
-from django.utils.translation import ugettext_noop
+from django.utils.translation import gettext_noop
 from django.core.validators import RegexValidator
 # Backwards compatible settings.AUTH_USER_MODEL
 USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
@@ -19,19 +19,19 @@ class ExtraInfo(models.Model):
 
     # [08/12/2022] removed these, but leaving comment since there'll still be
     # `api` in the existing data in db. This option was split into two.
-    # ('api', ugettext_noop('Asian / Pacific Islander')),
+    # ('api', gettext_noop('Asian / Pacific Islander')),
     ETHNIC_GROUPS = (
-        ('w', ugettext_noop('White')),
-        ('ba', ugettext_noop('Black or African American')),
-        ('na', ugettext_noop('American Indian or Alaska Native')),
-        ('as', ugettext_noop('Asian')),
-        ('nhpi', ugettext_noop('Native Hawaiian or Pacific Islander')),
-        ('hl', ugettext_noop('Hispanic or Latino/a')),
-        ('me', ugettext_noop('Middle Eastern')),
-        ('bm', ugettext_noop('Biracial or Multiracial')),
-        ('o', ugettext_noop('Other')),
-        # ('other', ugettext_noop('Other')), # TODO - change this to text field
-        ('prefer-not-to-say', ugettext_noop('Prefer not to say'))
+        ('w', gettext_noop('White')),
+        ('ba', gettext_noop('Black or African American')),
+        ('na', gettext_noop('American Indian or Alaska Native')),
+        ('as', gettext_noop('Asian')),
+        ('nhpi', gettext_noop('Native Hawaiian or Pacific Islander')),
+        ('hl', gettext_noop('Hispanic or Latino/a')),
+        ('me', gettext_noop('Middle Eastern')),
+        ('bm', gettext_noop('Biracial or Multiracial')),
+        ('o', gettext_noop('Other')),
+        # ('other', gettext_noop('Other')), # TODO - change this to text field
+        ('prefer-not-to-say', gettext_noop('Prefer not to say'))
     )
     ethnicity = models.CharField(
         verbose_name="Race or Ethnicity",
@@ -46,17 +46,17 @@ class ExtraInfo(models.Model):
             return self.__enumerable_to_display(self.ETHNIC_GROUPS, self.ethnicity)
 
     EMPLOYMENT_STATUS_CHOICES = (
-        ('efw', ugettext_noop('Employed for wages')),
-        ('selfemployed', ugettext_noop('Self-employed')),
-        ('student', ugettext_noop('Student')),
-        ('homemaker', ugettext_noop('Homemaker')),
-        ('oowlfw', ugettext_noop('Out of work and looking for work')),
-        ('oownclfw', ugettext_noop('Out of work but not currently looking for work')),
-        ('military', ugettext_noop('Military')),
-        ('retired', ugettext_noop('Retired')),
-        ('utw', ugettext_noop('Unable to work')),
-        # ('other', ugettext_noop('Other')), # TODO - change this to text field
-        ('prefer-not-to-say', ugettext_noop('Prefer not to say'))
+        ('efw', gettext_noop('Employed for wages')),
+        ('selfemployed', gettext_noop('Self-employed')),
+        ('student', gettext_noop('Student')),
+        ('homemaker', gettext_noop('Homemaker')),
+        ('oowlfw', gettext_noop('Out of work and looking for work')),
+        ('oownclfw', gettext_noop('Out of work but not currently looking for work')),
+        ('military', gettext_noop('Military')),
+        ('retired', gettext_noop('Retired')),
+        ('utw', gettext_noop('Unable to work')),
+        # ('other', gettext_noop('Other')), # TODO - change this to text field
+        ('prefer-not-to-say', gettext_noop('Prefer not to say'))
 
     )
     employment_status = models.CharField(
@@ -83,9 +83,9 @@ class ExtraInfo(models.Model):
     )
 
     ENROLLED_IN_SCHOOL_CHOICES = (
-        ('no', ugettext_noop('No')),
-        ('yes', ugettext_noop('Yes')),
-        ('prefer-not-to-say', ugettext_noop('Prefer not to say'))
+        ('no', gettext_noop('No')),
+        ('yes', gettext_noop('Yes')),
+        ('prefer-not-to-say', gettext_noop('Prefer not to say'))
     )
 
     enrolled_in_school = models.CharField(
@@ -103,13 +103,13 @@ class ExtraInfo(models.Model):
             return self.__enumerable_to_display(self.ENROLLED_IN_SCHOOL_CHOICES, self.enrolled_in_school)
 
     ENROLLED_IN_SCHOOL_TYPE_CHOICES = (
-        ('hs', ugettext_noop('High School')),
-        ('two-year', ugettext_noop('2-year/technical college')),
-        ('four-year', ugettext_noop('4-year college or university')),
-        ('grad', ugettext_noop('Graduate School')),
-        ('ne', ugettext_noop('Not Enrolled')),
-        # ('o', ugettext_noop('Other')),  # change this to text field input
-        ('prefer-not-to-say', ugettext_noop('Prefer not to say'))
+        ('hs', gettext_noop('High School')),
+        ('two-year', gettext_noop('2-year/technical college')),
+        ('four-year', gettext_noop('4-year college or university')),
+        ('grad', gettext_noop('Graduate School')),
+        ('ne', gettext_noop('Not Enrolled')),
+        # ('o', gettext_noop('Other')),  # change this to text field input
+        ('prefer-not-to-say', gettext_noop('Prefer not to say'))
     )
     enrolled_in_school_type = models.CharField(
         verbose_name="Type of Current School Enrollment",
@@ -126,11 +126,11 @@ class ExtraInfo(models.Model):
             return self.__enumerable_to_display(self.ENROLLED_IN_SCHOOL_TYPE_CHOICES, self.enrolled_in_school_type)
 
     LOCAL_COMMUNITY_LIVING_CHOICES = (
-        ('ls',ugettext_noop('A large city')),
-        ('su',ugettext_noop('A suburb near a large city')),
-        ('sct',ugettext_noop('A small city or town')),
-        ('ra',ugettext_noop('A rural area')),
-        ('prefer-not-to-say', ugettext_noop('Prefer not to say'))
+        ('ls',gettext_noop('A large city')),
+        ('su',gettext_noop('A suburb near a large city')),
+        ('sct',gettext_noop('A small city or town')),
+        ('ra',gettext_noop('A rural area')),
+        ('prefer-not-to-say', gettext_noop('Prefer not to say'))
     )
     local_community_living = models.CharField(
         verbose_name="Thinking about your local community, which of the following best describes the place you live now?",
