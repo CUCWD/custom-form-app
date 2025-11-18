80d1c2e60f35471503cbef5e5fb37b91f0d9ba70
Zachary Trabookis <ztraboo@clemson.edu>
2025-11-18 15:58:06 -0500

    fix: Bug on the registration page due to field error message being an object instead of a string.
    https://github.com/CUCWD/release-tracking/issues/66
    
    The frontend-app-authn React registration page was throwing an error on our custom registration form fields. It turns out the field error messages were being set to an object and React was expecting a string value.

--- custom_reg_form/forms.py
+++ custom_reg_form/forms.py
@@ -8,31 +8,92 @@ class ExtraInfoForm(ModelForm):
     """
     def __init__(self, *args, **kwargs):
         super(ExtraInfoForm, self).__init__(*args, **kwargs)
-        self.fields['ethnicity'].error_messages = {
-            "required": u"Please tell us your ethnicity",
-            "invalid": u"Enter correct ethnicity",
-        }
-        self.fields['employment_status'].error_messages = {
-            "required": u"Please tell us your employment status",
-            "invalid": u"Enter correct employment status",
-        }
-        self.fields['zipcode'].error_messages = {
-            "required": u"Please tell us your American ZIP Code",
-            "invalid": u"Invalid American Postal Code (nnnnn or nnnnn-nnnn)",
-        }
-        self.fields['enrolled_in_school'].error_messages = {
-            "required": u"Please tell us if you are enrolled in school",
-            "invalid": u"Enter correct school enrollment status",
-        }
-        self.fields['enrolled_in_school_type'].error_messages = {
-            "required": u"Please tell us about the type of school you are enrolled in",
-            "invalid": u"Enter correct school enrollment type",
-        }
-        self.fields['local_community_living'].error_messages = {
-            "required": u"Please tell us which of the following best describes the place you live now",
-            "invalid": u"Enter correct local community type",
-        }
 
+
+        # Ethnicity
+        # ----------------------------------
+
+        # Remove the Django serialized default of '---------' from the choices
+        field = self.fields['ethnicity']
+        # Keep it optional, but change the placeholder text
+        choices = list(field.choices)
+        if choices and choices[0][0] == "":
+            choices.remove(choices[0])
+        field.choices = choices
+
+        # This is where the error on the `frontend_app_authn` MFE registration form
+        # was causing the page to crash. Previously we had set this to an object.
+        # It just wants a string of dict of strings now.
+        self.fields['ethnicity'].error_messages = u"Select your ethnicity"
+
+
+        # Employment Status
+        # ----------------------------------
+
+        # Remove the Django serialized default of '---------' from the choices
+        field = self.fields['employment_status']
+        # Keep it optional, but change the placeholder text
+        choices = list(field.choices)
+        if choices and choices[0][0] == "":
+            choices.remove(choices[0])
+        field.choices = choices
+
+        # This is where the error on the `frontend_app_authn` MFE registration form
+        # was causing the page to crash. Previously we had set this to an object.
+        # It just wants a string of dict of strings now.
+        self.fields['employment_status'].error_messages = u"Select your employment status"
+        
+        # ZIP Code
+        # ----------------------------------
+        self.fields['zipcode'].error_messages = u"Please tell us your American ZIP Code"
+
+        # Enrolled in School
+        # ----------------------------------
+
+        # Remove the Django serialized default of '---------' from the choices
+        field = self.fields['enrolled_in_school']
+        # Keep it optional, but change the placeholder text
+        choices = list(field.choices)
+        if choices and choices[0][0] == "":
+            choices.remove(choices[0])
+        field.choices = choices
+
+        # This is where the error on the `frontend_app_authn` MFE registration form
+        # was causing the page to crash. Previously we had set this to an object.
+        # It just wants a string of dict of strings now.
+        self.fields['enrolled_in_school'].error_messages = u"Select your enrolled in school status"
+        
+        # Enrolled in School Type
+        # ----------------------------------
+
+        # Remove the Django serialized default of '---------' from the choices
+        field = self.fields['enrolled_in_school_type']
+        # Keep it optional, but change the placeholder text
+        choices = list(field.choices)
+        if choices and choices[0][0] == "":
+            choices.remove(choices[0])
+        field.choices = choices
+
+        # This is where the error on the `frontend_app_authn` MFE registration form
+        # was causing the page to crash. Previously we had set this to an object.
+        # It just wants a string of dict of strings now.
+        self.fields['enrolled_in_school_type'].error_messages = u"Select the type of school you are enrolled in"
+        
+        # Local Community Living
+        # ----------------------------------
+
+        # Remove the Django serialized default of '---------' from the choices
+        field = self.fields['local_community_living']
+        # Keep it optional, but change the placeholder text
+        choices = list(field.choices)
+        if choices and choices[0][0] == "":
+            choices.remove(choices[0])
+        field.choices = choices
+
+        # This is where the error on the `frontend_app_authn` MFE registration form
+        # was causing the page to crash. Previously we had set this to an object.
+        # It just wants a string of dict of strings now.
+        self.fields['local_community_living'].error_messages = u"Select the place that describes where you live now"
 
     class Meta(object):
         model = ExtraInfo
